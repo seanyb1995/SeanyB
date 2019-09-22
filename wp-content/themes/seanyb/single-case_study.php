0551a39adffb995b5619e733f992774425937284
sean_buchanan1995 <sean_buchanan1995@outlook.com>
2019-09-22 01:13:59 -0400

    portfolio-sm

--- wp-content/themes/seanyb/single-case_study.php
+++ wp-content/themes/seanyb/single-case_study.php
@@ -119,30 +119,7 @@ get_header();
                 </div>
                 <!--project video render-->
                 <div class="project-video">
-                 <?php 
-                 //Get the images ids from the post_metadata
-                    $images = acf_photo_gallery('video', $post->ID);
-                    //Check if return array has anything in it
-                    if( count($images) ):
-                        //Cool, we got some data so now let's loop over it
-                        foreach($images as $image):
-                            $id = $image['id']; // The attachment id of the media
-                            $title = $image['title']; //The title
-                            $caption= $image['caption']; //The caption
-                            $full_image_url= $image['full_image_url']; //Full size image url
-                            $full_image_url = acf_photo_gallery_resize_image($full_image_url, 960, 642); //Resized size to 262px width by 160px height image url
-                            $thumbnail_image_url= $image['thumbnail_image_url']; //Get the thumbnail size image url 150px by 150px
-                            $url= $image['url']; //Goto any link when clicked
-                            $target= $image['target']; //Open normal or new tab
-                            $alt = get_field('photo_gallery_alt', $id); //Get the alt which is a extra field (See below how to add extra fields)
-                            $class = get_field('photo_gallery_class', $id); //Get the class which is a extra field (See below how to add extra fields)
-                  ?>
-                  <?php if( !empty($url) ){ ?><a href="<?php echo $url; ?>" <?php echo ($target == 'true' )? 'target="_blank"': ''; ?>><?php } ?>
-                  <figure>
-                    <img src="<?php echo $full_image_url; ?>" alt="<?php echo $title; ?>" title="<?php echo $title; ?>">
-                  </figure>
-                  <?php if( !empty($url) ){ ?></a><?php } ?>
-                  <?php endforeach; endif; ?>
+                  <video src="/wp-content/themes/seanyb/videos/showreel.mkv" type="video/x-matroska;" controls></video>
                 </div>
                 <!--project process-->
                 <div class="project-process">
